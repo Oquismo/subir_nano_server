418336269c9d05e7be906299e3493399cfd54c90
Stefano <161383899+Oquismo@users.noreply.github.com>
2024-11-01 05:16:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-const express  = require ('express');
-const bunyan = require ('bunyan');
-const cors = require ('cors');
+// Importando el módulo express para crear una aplicación web
 
-require('dotenv').config();
+const express = require("express");
+// Importando el módulo bunyan para el registro de eventos
 
+const bunyan = require("bunyan");
 
-const usuarios = require ('./router/usuariosRouter');
-const escuderia = require ('./router/escuderiaRouter');
-const pilotos = require ('./router/pilotosRouter');
-const Logger = require('bunyan');
+// Importando el módulo cors para permitir solicitudes desde diferentes orígenes
 
+const cors = require("cors");
 
+// Importando el módulo dotenv para cargar variables de entorno
 
-const logger = bunyan.createLogger({name: 'Servidor'});
+require("dotenv").config();
+
+// Importando los controladores de rutas para las escuderías, pilotos y usuarios
+
+const usuarios = require("./router/usuariosRouter");
+const escuderia = require("./router/escuderiaRouter");
+const pilotos = require("./router/pilotosRouter");
+
+const Logger = require("bunyan");
+
+// Creando un registrador de eventos para la aplicación
+
+const logger = bunyan.createLogger({ name: "Servidor" });
 
 const app = express();
 
+// Configurando la aplicación para permitir solicitudes desde diferentes orígenes
+
 app.use(cors());
+
+// Configurando la aplicación para interpretar datos JSON en los cuerpos de las solicitudes
 
 app.use(express.json());
 
+// Agregando las rutas de las escuderías, pilotos y usuarios a la aplicación
 
-app.use('/usuarios', usuarios);
-app.use('/escuderia', escuderia);
-app.use('/pilotos', pilotos);
+app.use("/usuarios", usuarios);
+app.use("/escuderia", escuderia);
+app.use("/pilotos", pilotos);
 
+// Iniciando la aplicación en el puerto especificado en las variables de entorno
 
 app.listen(process.env.PUERTO, () => {
-    logger.info('Servidor levantado');
-})
+  // Registrando un evento de información cuando la aplicación se inicie
+
+  logger.info("Servidor levantado");
+});

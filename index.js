f0ce5ef7d49b0f54f1e9ecd91c8a04a6a6722688
Stefano <161383899+Oquismo@users.noreply.github.com>
2025-01-13 23:44:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
 // Importando el módulo express para crear una aplicación web
-
 const express = require("express");
-// Importando el módulo bunyan para el registro de eventos
 
+// Importando el módulo bunyan para el registro de eventos
 const bunyan = require("bunyan");
 
 // Importando el módulo cors para permitir solicitudes desde diferentes orígenes
-
 const cors = require("cors");
 
 // Importando el módulo dotenv para cargar variables de entorno
-
 require("dotenv").config();
 
-// Importando los controladores de rutas para las escuderías, pilotos y usuarios
-
+// Importando los controladores de rutas para las escuderías, pilotos, usuarios y comentarios
 const usuarios = require("./router/usuariosRouter");
 const escuderia = require("./router/escuderiaRouter");
 const pilotos = require("./router/pilotosRouter");
-
-const Logger = require("bunyan");
+const comentarios = require("./router/comentariosRouter");  // Asegurándonos de que esta ruta esté incluida
 
 // Creando un registrador de eventos para la aplicación
-
 const logger = bunyan.createLogger({ name: "Servidor" });
 
 const app = express();
 
 // Configurando la aplicación para permitir solicitudes desde diferentes orígenes
-
 app.use(cors());
 
 // Configurando la aplicación para interpretar datos JSON en los cuerpos de las solicitudes
-
 app.use(express.json());
 
-// Agregando las rutas de las escuderías, pilotos y usuarios a la aplicación
-
+// Agregando las rutas de las escuderías, pilotos, usuarios y comentarios a la aplicación
 app.use("/usuarios", usuarios);
 app.use("/escuderia", escuderia);
 app.use("/pilotos", pilotos);
+app.use("/comentarios", comentarios);  // Aquí agregamos la ruta para los comentarios
 
 // Iniciando la aplicación en el puerto especificado en las variables de entorno
-
 app.listen(process.env.PUERTO, () => {
   // Registrando un evento de información cuando la aplicación se inicie
-
   logger.info("Servidor levantado");
 });
+
--- router/comentariosRouter.js
+++ router/comentariosRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Controlador de comentarios, donde se manejarán las lógicas de los comentarios.
-const comentariosController = require("../controllers/comentariosController");
+const comentariosController = require("../controller/comentariosController");
 
 // Ruta para agregar un comentario
 router.post("/", comentariosController.agregarComentario);

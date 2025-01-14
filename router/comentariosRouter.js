b75727682fc7055100c9234c03b060e0234b1929
Stefano <161383899+Oquismo@users.noreply.github.com>
2025-01-14 01:07:36 +0100

    Add files via upload

--- router/comentariosRouter.js
+++ router/comentariosRouter.js
@@ -7,9 +7,6 @@ const comentariosController = require("../controller/comentariosController");
 // Ruta para agregar un comentario
 router.post("/", comentariosController.agregarComentario);
 
-// Ruta para obtener todos los comentarios
-router.get("/", comentariosController.obtenerComentarios);
-
 // Puedes agregar más rutas aquí en el futuro si lo necesitas (por ejemplo, obtener comentarios, eliminar comentarios, etc.).
 
 module.exports = router;
